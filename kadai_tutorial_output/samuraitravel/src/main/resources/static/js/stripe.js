f2294491d12f5446dd10f3c3cbf18fb7de906d02
kazutaka-777 <cappuccino.kurage@gmail.com>
2025-04-22 01:37:00 +0900

    Delete kadai_tutorial_output/samuraitravel directory
    手直し削除

--- kadai_tutorial_output/samuraitravel/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51QyyTbQh8lgVIuOFfzoTmeaKJSxReBc71nrP94r7GhuoezVPHU3PIlqcxAAm1M2tJ1PAzB6L2lm1iykGaNo4S08000skxBSCoG');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
